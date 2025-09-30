2906c28ad84938524418ecc223a2c6ee5c4eceda
RainyXeon <rainyxeon@gmail.com>
2025-09-30 13:13:04 +0700

    add: script to repo

---
+++ scripts/exp-version.sh
@@ -0,0 +1,10 @@
+echo "Remove Official Local Version String"
+rm "localversion-moon"
+echo "Add experimental version string"
+echo "-EXPERIMENTAL-MoonWake" > localversion-00-experimental
+echo "Add commit version string"
+echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
+echo "Add run number string"
+echo "-$RUN_NUMBER" > localversion-02-experimental
+make O=out kernelrelease
+echo "Done!"
