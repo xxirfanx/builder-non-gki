92d00502e0813e33252051788277b3641bcd661a
RainyXeon <rainyxeon@gmail.com>
2025-10-09 16:36:25 +0700

    add: custom build for rainy and aeron

---
+++ scripts/aeron.sh
@@ -0,0 +1,6 @@
+echo "Do additional patches"
+curl -LSs "https://github.com/kernel-build-from-rainyland/builder-non-gki/raw/main/scripts/sps.sh"
+echo "Remove Official Local Version String"
+rm "localversion-moon"
+echo "Add: -Aeron-Rom_Fucker kernel string"
+echo "-Aeron-Rom_Fucker" > localversion-aeron
