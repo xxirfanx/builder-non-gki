439e5111248b18019354680f08c1130696f5cab3
Renia in RainyLand <rainyxeon@gmail.com>
2025-10-15 13:29:07 +0700

    Update rainy.sh

--- scripts/rainy.sh
+++ scripts/rainy.sh
@@ -5,3 +5,13 @@ cat > ./arch/arm64/configs/vendor/kpm.config <<'EOF'
 CONFIG_KPM=n
 EOF
 make ARCH=arm64 O=$OUT_DIR vendor/kpm.config
+echo "Remove Official Local Version String"
+rm "localversion-moon"
+echo "Add experimental version string"
+echo "-MoonWake-CI" > localversion-00-experimental
+echo "Add commit version string"
+echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
+echo "Add run number string"
+echo "-$RUN_NUMBER" > localversion-02-experimental
+make O=out kernelrelease
+echo "Done!"
