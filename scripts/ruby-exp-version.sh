1d84a4f553b0dfca62d5c14d5cb643145f071979
Renia in RainyLand <rainyxeon@gmail.com>
2025-10-15 13:29:21 +0700

    Update ruby-exp-version.sh

--- scripts/ruby-exp-version.sh
+++ scripts/ruby-exp-version.sh
@@ -1,7 +1,7 @@
 echo "Remove Official Local Version String"
 rm "localversion-moon"
 echo "Add experimental version string"
-echo "-EXPERIMENTAL-MoonWake" > localversion-00-experimental
+echo "-MoonWake-CI" > localversion-00-experimental
 echo "Add commit version string"
 echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
 echo "Add run number string"
