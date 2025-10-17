fcaa3601a2e6295125de5801b490e6bc66fb3290
RainyXeon <rainyxeon@gmail.com>
2025-10-17 13:06:28 +0700

    add: MoonWakeX

--- scripts/rainy.sh
+++ scripts/rainy.sh
@@ -5,13 +5,5 @@ cat > ./arch/arm64/configs/vendor/kpm.config <<'EOF'
 CONFIG_KPM=n
 EOF
 make ARCH=arm64 O=$OUT_DIR vendor/kpm.config
-echo "Remove Official Local Version String"
-rm "localversion-moon"
-echo "Add experimental version string"
-echo "-MoonWake-CI" > localversion-00-experimental
-echo "Add commit version string"
-echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
-echo "Add run number string"
-echo "-$RUN_NUMBER" > localversion-02-experimental
-make O=out kernelrelease
-echo "Done!"
+echo "Rename Official Local Version String to MoonWakeX"
+sed -i 's/-MoonWake-\([0-9.]\+\)/-MoonWakeX-\1/' localversion-moon
