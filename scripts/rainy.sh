3f4984c842ee15e31578164d9b88aa981a567e29
Renia in RainyLand <rainyxeon@gmail.com>
2025-10-18 20:29:33 +0700

    renable clang
    Remove KPM configuration and adjust build settings.

--- scripts/rainy.sh
+++ scripts/rainy.sh
@@ -1,9 +1,8 @@
 echo "Do additional patches"
 curl -LSs "https://github.com/kernel-build-from-rainyland/builder-non-gki/raw/main/scripts/sps.sh" | bash
 echo "Remove KPM"
-cat > ./arch/arm64/configs/vendor/kpm.config <<'EOF'
-CONFIG_KPM=n
-EOF
-make ARCH=arm64 O=$OUT_DIR vendor/kpm.config
+scripts/config --enable LTO_CLANG
+scripts/config --disable LTO_NONE
+scripts/config --disable KPM
 echo "Rename Official Local Version String to MoonWakeX"
 sed -i 's/-MoonWake-\([0-9.]\+\)/-MoonWakeX-\1/' localversion-moon
