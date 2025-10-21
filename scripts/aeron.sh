3539d72b968d9570f30a6050dc693d7bcbd47559
RainyXeon <rainyxeon@gmail.com>
2025-10-21 21:31:13 +0700

    add: aeron kernel

--- scripts/aeron.sh
+++ scripts/aeron.sh
@@ -4,8 +4,3 @@ echo "Remove Official Local Version String"
 rm "localversion-moon"
 echo "Add: -Aeron-Rom_Fucker kernel string"
 echo "-Aeron-Rom_Fucker" > localversion-aeron
-echo "Remove KPM"
-cat > ./arch/arm64/configs/vendor/kpm.config <<'EOF'
-CONFIG_KPM=n
-EOF
-make ARCH=arm64 O=$OUT_DIR vendor/kpm.config
